93eca97fab9889dc56ed519be39bdf1955ef39aa
Tanya <tanyalykina57@gmail.com>
2022-12-16 21:21:35 +0500

    changes

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,16 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
-        List<Person> clients = generateClients();
-        Collections.sort(clients, new PersonComparator().reversed());
+            ArrayList<Person> clients = new ArrayList<>();
+            clients.add(new Person("Билл", "Гейтс", 5));
+            clients.add(new Person("Макс", "Стародубский Ромодановский Пивоваренный Сыроед", 20));
+            clients.add(new Person("Джефф", "Безос", 10));
+            clients.add(new Person("Джастин", "Римский-Корсаков", 30));
+            clients.add(new Person("Петр", "Михайловский Данилевский", 25));
+            clients.add(new Person("!Роберт", "Тэппан-Моррис", 15));
+            clients.add(new Person("!Геральд", "Муррэй Моррис", 40));
+            clients.add(new Person("Пикассо", "Пабло Диего Хозе Франциско де Паула Хуан", 35));
+            clients.sort(new PersonComparator(5));
         System.out.println(clients);
+        }
     }
-
-    public static List<Person> generateClients() {
-        List<Person> clients = new ArrayList<>();
-        clients.add(new Person("Билл", "Гейтс", 5));
-        clients.add(new Person("Макс", "Стародубский Ромодановский Пивоваренный Сыроед", 20));
-        clients.add(new Person("Джефф", "Безос", 10));
-        clients.add(new Person("Джастин", "Таннер Петерсен", 30));
-        clients.add(new Person("Петр", "Михайловский Данилевский", 25));
-        clients.add(new Person("!Роберт", "Тэппан Моррис", 15));
-        clients.add(new Person("!Геральд", "Муррэй Моррис", 40));
-        clients.add(new Person("Пикассо", "Пабло Диего Хозе Франциско де Паула Хуан", 35));
-        return clients;
-    }
-}
